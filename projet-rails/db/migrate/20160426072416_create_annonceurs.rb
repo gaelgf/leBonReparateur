1720386303e4d4f04894a0ef78b9ec7175c87a3e
bro <bro@zengularity.com>
2016-05-25 20:03:08 +0200

    devise

--- projet-rails/db/migrate/20160426072416_create_annonceurs.rb
+++ projet-rails/db/migrate/20160426072416_create_annonceurs.rb
@@ -1,10 +1,8 @@
 class CreateAnnonceurs < ActiveRecord::Migration
   def change
     create_table :annonceurs do |t|
-      t.string :email
       t.string :nom
       t.string :prenom
-      t.string :password
       t.string :cp
       t.string :adresse
       t.string :telephone
--- projet-rails/db/schema.rb
+++ projet-rails/db/schema.rb
@@ -11,13 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524140400) do
+ActiveRecord::Schema.define(version: 20160525155050) do
 
   create_table "annonceurs", force: :cascade do |t|
-    t.string   "email"
     t.string   "nom"
     t.string   "prenom"
-    t.string   "password"
     t.string   "cp"
     t.string   "adresse"
     t.string   "telephone"
@@ -43,5 +41,30 @@ ActiveRecord::Schema.define(version: 20160524140400) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "nom"
+    t.string   "prenom"
+    t.string   "cp"
+    t.string   "adresse"
+    t.string   "telephone"
+    t.string   "situation"
+    t.string   "rayonIntervention"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
