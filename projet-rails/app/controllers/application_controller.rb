0bcea70570b8dc103cc3a3717be815b70cd2bd46
bro <bro@zengularity.com>
2016-07-13 16:12:04 +0200

    add paperclip in devise

--- projet-rails/app/controllers/application_controller.rb
+++ projet-rails/app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nom, :prenom, :cp, :adresse, :telephone, :rayonIntervention, :situation, :description, :email, :password, :avatar) }
+        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :current_password, :nom, :prenom, :cp, :adresse, :telephone, :rayonIntervention, :situation, :description, :avatar) }
+    end
 end
--- projet-rails/app/controllers/registrations_controller.rb
+++ projet-rails/app/controllers/registrations_controller.rb
@@ -2,10 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
     def sign_up_params
-      params.require(:user).permit(:nom, :prenom, :cp, :adresse, :telephone, :rayonIntervention, :situation, :description, :email, :password, :password_confirmation)
+      params.require(:user).permit(:nom, :prenom, :cp, :adresse, :telephone, :rayonIntervention, :situation, :description, :avatar, :email, :password, :password_confirmation)
     end
 
     def account_update_params
-      params.require(:user).permit(:nom, :prenom, :cp, :adresse, :telephone, :situation, :rayonIntervention, :description, :email, :password, :password_confirmation, :current_password)
+      params.require(:user).permit(:nom, :prenom, :cp, :adresse, :telephone, :situation, :rayonIntervention, :description, :avatar, :email, :password, :password_confirmation, :current_password)
     end
 end
