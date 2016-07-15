837030bee7099e80742ccb902d98fc6b974bf3df
bro <bro@zengularity.com>
2016-07-15 10:46:21 +0200

    update search for search with cp or city and stylize it

--- projet-rails/app/models/user.rb
+++ projet-rails/app/models/user.rb
@@ -10,6 +10,6 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   search_scope :search do
-		attributes :cp
+		attributes :cp, :adresse
 	end
 end
